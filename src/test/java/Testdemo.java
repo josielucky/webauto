30192e54a2f4d64b93f78bdef51d77e377c87b0e
josie <josie2@126.com>
2022-11-06 16:53:55 +0800

    文件夹中修改了testdemoTestdemo.java

--- src/test/java/Testdemo.java
+++ src/test/java/Testdemo.java
@@ -1,5 +1,6 @@
 public class Testdemo {
     public static void main(String[] args) throws InterruptedException {
         System.out.println("增加类testdemo");
+		System.out.println("修改testdemo");
     }
 }
