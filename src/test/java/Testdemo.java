d3232cc217c9e03866affa5f17f510ba3fc1b252
josie <josie2@126.com>
2022-11-06 17:02:29 +0800

    文件夹另一个工作区修改了testdemo

--- src/test/java/Testdemo.java
+++ src/test/java/Testdemo.java
@@ -1,6 +1,6 @@
 public class Testdemo {
     public static void main(String[] args) throws InterruptedException {
         System.out.println("增加类testdemo");
-		System.out.println("修改testdemo");
+		System.out.println("文件夹另一个工作区修改testdemo");
     }
 }
